b4517bf1845142bdf204ed5c79d564fbd75a0871
mohamed55247 <mohamed20182005ii@gmail.com>
2025-03-15 06:33:17 -0800

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector(".img");
+let contianer = document.querySelector(".contianer");
+
+function phones(phone){
+    img.src = phone;
+}
+
+function color(color){
+    contianer.style.backgroundColor = color;
+}
